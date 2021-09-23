945cd59266e4c83ea32ebfe45b78daea383af01f
manavparikh01 <manavparikh01@gmail.com>
2021-09-23 17:16:21 +0530

    signinwithgoogleadded

--- prooject/src/Login.js
+++ prooject/src/Login.js
@@ -3,6 +3,8 @@ import { withRouter, Redirect } from "react-router";
 import app from "./base.js";
 import { AuthContext } from "./Auth.js";
 import UserDataContext from "./Context/credentialscontext.js";
+import * as firebase from 'firebase';
+
 
 const Login = ({ history }) => {
   const userData=useContext(UserDataContext)
@@ -11,6 +13,9 @@ const Login = ({ history }) => {
       event.preventDefault();
       const { email, password } = event.target.elements;
       try {
+
+      
+
         await app
           .auth()
           .signInWithEmailAndPassword(email.value, password.value);
@@ -30,6 +35,24 @@ const Login = ({ history }) => {
     return <Redirect to="/" />;
   }
 
+
+  const googleSignIn = async () => {
+    var provider = new firebase.auth.GoogleAuthProvider()
+    // const provider = app.auth.GoogleAuthProvider()
+    await app.auth().signInWithPopup(provider)
+    .then((re) => {
+      console.log(re);
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+
+  }
+
+  const signUpInstead = () => {
+    history.push('/signup');
+  }
+
   return (
     <div>
       <h1>Log in</h1>
@@ -43,7 +66,17 @@ const Login = ({ history }) => {
           <input name="password" type="password" placeholder="Password" />
         </label>
         <button type="submit">Log in</button>
+        
       </form>
+      <div>
+      <button onClick={signUpInstead}>Sign up with email</button>
+      </div>
+
+      <div>
+      <button onClick={googleSignIn}>
+        Sign In with Google
+      </button>
+    </div>
     </div>
   );
 };
